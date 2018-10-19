909eeb0e900d4de89ed98a6dae03d17c9c4618f4
Kilkee <39921163+Kilkee@users.noreply.github.com>
2018-10-20 00:14:51 +0100

    Basic menu usage

--- JerseyLab/app/src/main/java/com/example/delaney/jerseylab/MainActivity.java
+++ JerseyLab/app/src/main/java/com/example/delaney/jerseylab/MainActivity.java
@@ -1,6 +1,8 @@
 package com.example.delaney.jerseylab;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.provider.Settings;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
@@ -64,14 +66,15 @@ public class MainActivity extends AppCompatActivity {
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        //TODO: (PD - 02  -7.48) This is the function that gets called when you press a menu item
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
+        switch (item.getItemId()){
+            case R.id.action_reset:
+                mCurrentItem = new Item();
+                showCurrentItem();
+                return true;
+            case R.id.action_settings:
+               // startActivity(new Intent(Settings.ACTION_SETTINGS));
+                startActivity(new Intent(Settings.ACTION_LOCALE_SETTINGS));
 
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
             return true;
         }
 
